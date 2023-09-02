f875c4b07e02e076373bd2dedb49d553edbfdc23
Talimul Islam <136077674+talimulislam05@users.noreply.github.com>
2023-09-02 19:28:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//text change
+function textChange(){
+    document.getElementById('h2').innerHTML="Hello Bangladesh";
+}
+
+function textChangeh2(){
+    document.getElementById('h2_1').innerHTML="Hello World";
+}
+
+function textChangeh2_1(){
+    document.getElementById('h2_2').innerHTML="Hello Munshiganj";
+}
+
+function textChangeh2_2(){
+    document.getElementById('h2_3').innerHTML="Hello Sreenagar";
+}
+
+function textChangeh2_3(){
+    document.getElementById('h2_4').innerHTML="Hello Bhagyakul";
+}
